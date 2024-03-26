704f23cca234493523db71838664e78f651cd91f
MurselOzzobuoglu <12203170@student.pxl.be>
2024-03-26 10:05:35 +0100

    TestRsa

--- EncryptieTool/Views/RSA_First.xaml.cs
+++ EncryptieTool/Views/RSA_First.xaml.cs
@@ -1,40 +1,98 @@
 ﻿using KeysLibrary;
+using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace EncryptieTool.Views
 {
-    /// <summary>
-    /// Interaction logic for RSA_First.xaml
-    /// </summary>
-    public partial class RSA_First : Window
-    {
-        public RSA_First()
-        {
-            InitializeComponent();
-        }
+	public partial class RSA_First : Window
+	{
+		public RSA_First()
+		{
+			InitializeComponent();
+		}
 
-        private void BtnUseRSA_Click(object sender, RoutedEventArgs e)
-        {
-            ChosenKey.Method = "Use";
+		private void BtnUseRSA_Click(object sender, RoutedEventArgs e)
+		{
+			// Implementatie voor het gebruik van een bestaande RSA-sleutel
+			ChosenKey.Method = "Use";
+			MessageBox.Show("Gebruik een bestaande RSA-sleutel", "Informatie", MessageBoxButton.OK, MessageBoxImage.Information);
+		}
 
-        }
+		private void BtnGenRSA_Click(object sender, RoutedEventArgs e)
+		{
+			// Implementatie voor het genereren van een nieuwe RSA-sleutel
+			ChosenKey.Method = "Gen";
 
-        private void BtnGenRSA_Click(object sender, RoutedEventArgs e)
-        {
-            ChosenKey.Method = "Gen";
+			var rsa = new CryptingRSA();
+			var (publicKey, privateKey) = rsa.GenerateRSAKeyPair();
 
-        }
-    }
+			// Genereer een unieke bestandsnaam op basis van de huidige tijd
+			string timestamp = DateTime.Now.ToString("yyyyMMddHHmmss");
+			string publicKeyFilename = $"RSA_PublicKey_{timestamp}.xml";
+			string privateKeyFilename = $"RSA_PrivateKey_{timestamp}.xml";
+
+			// Stel een standaardlocatie in om de sleutels op te slaan, bijv. in de map Documenten
+			string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+			string fullPathPublic = Path.Combine(folderPath, publicKeyFilename);
+			string fullPathPrivate = Path.Combine(folderPath, privateKeyFilename);
+
+			// Sla de sleutels op
+			File.WriteAllText(fullPathPublic, publicKey);
+			File.WriteAllText(fullPathPrivate, privateKey);
+
+			MessageBox.Show($"RSA-sleutelpaar gegenereerd en opgeslagen als:\n{fullPathPublic}\n{fullPathPrivate}", "RSA Sleutelpaar", MessageBoxButton.OK, MessageBoxImage.Information);
+		}
+
+		private string ChoosePngFile()
+		{
+			var openFileDialog = new Microsoft.Win32.OpenFileDialog
+			{
+				Filter = "PNG Files (*.png)|*.png",
+				Title = "Selecteer een PNG-bestand"
+			};
+
+			bool? result = openFileDialog.ShowDialog();
+			if (result == true)
+			{
+				return openFileDialog.FileName;
+			}
+
+			return null;
+		}
+
+		private void BtnEncryptAndSave_Click(object sender, RoutedEventArgs e)
+		{
+			string pngFilePath = ChoosePngFile();
+			if (pngFilePath == null)
+			{
+				MessageBox.Show("Geen bestand geselecteerd.", "Fout", MessageBoxButton.OK, MessageBoxImage.Error);
+				return;
+			}
+
+			// Genereer RSA-sleutelpaar
+			var rsa = new CryptingRSA();
+			var (publicKey, privateKey) = rsa.GenerateRSAKeyPair();
+
+			// Versleutel de inhoud van het bestand (voor de demo versleutelen we een placeholder tekst)
+			var encryptedData = rsa.EncryptData($"Versleutelde inhoud van {Path.GetFileName(pngFilePath)}", publicKey);
+			string encryptedContentBase64 = Convert.ToBase64String(encryptedData);
+
+			// Definieer de map waarin je de bestanden wilt opslaan
+			string folderPath = @"C:\EncryptedPNGs";
+			if (!Directory.Exists(folderPath))
+			{
+				Directory.CreateDirectory(folderPath);
+			}
+
+			// Sla de versleutelde inhoud en de sleutels op
+			string timestamp = DateTime.Now.ToString("yyyyMMddHHmmss");
+			File.WriteAllText(Path.Combine(folderPath, $"encrypted_{timestamp}.txt"), encryptedContentBase64);
+			File.WriteAllText(Path.Combine(folderPath, $"publicKey_{timestamp}.xml"), publicKey);
+			File.WriteAllText(Path.Combine(folderPath, $"privateKey_{timestamp}.xml"), privateKey);
+
+			MessageBox.Show($"Versleuteling voltooid en bestanden opgeslagen in {folderPath}.", "Succes", MessageBoxButton.OK, MessageBoxImage.Information);
+		}
+	}
 }
--- KeysLibrary/CryptingRSA.cs
+++ KeysLibrary/CryptingRSA.cs
@@ -1,12 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace KeysLibrary
 {
-    internal class CryptingRSA
+    public class CryptingRSA
     {
+        private readonly int KeySize = 2048;
+
+        public (string publicKey, string privateKey) GenerateRSAKeyPair()
+        {
+            using (var rsa = new RSACryptoServiceProvider(KeySize))
+            {
+                rsa.PersistKeyInCsp = false;
+                var publicKey = rsa.ToXmlString(false);
+                var privateKey = rsa.ToXmlString(true);
+                return (publicKey, privateKey);
+            }
+        }
+
+        public byte[] EncryptData(string dataToEncrypt, string publicKey)
+        {
+            using (var rsa = new RSACryptoServiceProvider(KeySize))
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.FromXmlString(publicKey);
+                var encryptedData = rsa.Encrypt(Encoding.UTF8.GetBytes(dataToEncrypt), true);
+                return encryptedData;
+            }
+        }
+
+        public string DecryptData(byte[] dataToDecrypt, string privateKey)
+        {
+            using (var rsa = new RSACryptoServiceProvider(KeySize))
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.FromXmlString(privateKey);
+                var decryptedData = rsa.Decrypt(dataToDecrypt, true);
+                return Encoding.UTF8.GetString(decryptedData);
+            }
+        }
     }
 }
