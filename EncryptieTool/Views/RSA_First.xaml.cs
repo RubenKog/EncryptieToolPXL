25dae19b8a3aeea29ddb057f7f13c8a49e6d69a3
MurselOzzobuoglu <12203170@student.pxl.be>
2024-03-27 17:41:43 +0100

    Eerste menu

--- EncryptieTool/Views/RSA_First.xaml.cs
+++ EncryptieTool/Views/RSA_First.xaml.cs
@@ -8,12 +8,15 @@ using KeysLibrary;
 using System;
 
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
