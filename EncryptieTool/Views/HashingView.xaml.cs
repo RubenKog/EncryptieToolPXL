427d375cb3a43c46f326e120a2c9f741ce6e74d7
jasperorens <115090529+jasperorens@users.noreply.github.com>
2024-04-06 21:15:20 +0200

    UPDATE: added Blake Hashing to validation screen

--- EncryptieTool/Views/HashingView.xaml.cs
+++ EncryptieTool/Views/HashingView.xaml.cs
@@ -24,6 +24,7 @@ namespace EncryptieTool.Views
     {
         string File1;
         string File2;
+        public string HashingAlgor { get; set; } = "SHA256";
         public HashingView()
         {
             File1 = null;
@@ -36,37 +37,81 @@ namespace EncryptieTool.Views
             try
             {
                 Hashing hashing = new Hashing();
-                byte[] SHA256File1 = hashing.FileToHash(File1);
-                byte[] SHA256File2 = hashing.FileToHash(File2);
-                string SHA256String1 = Convert.ToBase64String(SHA256File1);
-                string SHA256String2 = Convert.ToBase64String(SHA256File2);
                 StringBuilder sb = new StringBuilder();
-                sb.AppendLine("Checking integrity through SHA256 hash:");
-                if (SHA256File1 == null || SHA256File2 == null)
+                byte[] HashedFile1;
+                byte[] HashedFile2;
+                if (HashingAlgor == "SHA256") 
                 {
-                    MessageBox.Show("Please select two files.");
-                }
-                else if (SHA256File1 != null && SHA256File2 != null && SHA256String1 == SHA256String2)
-                {
-                    sb.AppendLine("");
-                    sb.AppendLine($"File 1 hash: {SHA256String1}");
-                    sb.AppendLine("");
-                    sb.AppendLine($"File 2 hash: {SHA256String2}");
-                    sb.AppendLine("");
-                    sb.AppendLine("Integrity check complete: Success!");
-                    MessageBox.Show(sb.ToString(), "Ontegrity Checker");
+                    HashedFile1 = hashing.FileToHashSHA(File1);
+                    HashedFile2 = hashing.FileToHashSHA(File2);
+                    string SHA256String1 = Convert.ToBase64String(HashedFile1);
+                    string SHA256String2 = Convert.ToBase64String(HashedFile2);
+
+                    sb.AppendLine("Checking integrity through SHA256 hash:");
+                    if (HashedFile1 == null || HashedFile2 == null)
+                    {
+                        MessageBox.Show("Please select two files.");
+                    }
+                    else if (HashedFile1 != null && HashedFile2 != null && SHA256String1 == SHA256String2)
+                    {
+                        sb.AppendLine("");
+                        sb.AppendLine($"File 1 hash: {SHA256String1}");
+                        sb.AppendLine("");
+                        sb.AppendLine($"File 2 hash: {SHA256String2}");
+                        sb.AppendLine("");
+                        sb.AppendLine("Integrity check complete: Success!");
+                        MessageBox.Show(sb.ToString(), "Ontegrity Checker");
+                    }
+                    else
+                    {
+                        sb.AppendLine("");
+                        sb.AppendLine($"File 1 hash: {SHA256String1}");
+                        sb.AppendLine("");
+                        sb.AppendLine($"File 2 hash: {SHA256String2}");
+                        sb.AppendLine("");
+                        sb.AppendLine("Integrity check complete: Failure!");
+                        MessageBox.Show(sb.ToString(), "Integrity Checker");
+                    }
+                    sb.Clear();
                 }
                 else
                 {
-                    sb.AppendLine("");
-                    sb.AppendLine($"File 1 hash: {SHA256String1}");
-                    sb.AppendLine("");
-                    sb.AppendLine($"File 2 hash: {SHA256String2}");
-                    sb.AppendLine("");
-                    sb.AppendLine("Integrity check complete: Failure!");
-                    MessageBox.Show(sb.ToString(), "Integrity Checker");
+                    HashedFile1 = hashing.FileToHashBLAKE(File1);
+                    HashedFile2 = hashing.FileToHashBLAKE(File2);
+                    string BlakeString1 = Convert.ToBase64String(HashedFile1);
+                    string BlakeString2 = Convert.ToBase64String(HashedFile2);
+
+                    sb.AppendLine("Checking integrity through BLAKE hash:");
+                    if (HashedFile1 == null || HashedFile2 == null)
+                    {
+                        MessageBox.Show("Please select two files.");
+                    }
+                    else if (HashedFile1 != null && HashedFile2 != null && BlakeString1 == BlakeString2)
+                    {
+                        sb.AppendLine("");
+                        sb.AppendLine($"File 1 hash: {BlakeString1}");
+                        sb.AppendLine("");
+                        sb.AppendLine($"File 2 hash: {BlakeString2}");
+                        sb.AppendLine("");
+                        sb.AppendLine("Integrity check complete: Success!");
+                        MessageBox.Show(sb.ToString(), "Ontegrity Checker");
+                    }
+                    else
+                    {
+                        sb.AppendLine("");
+                        sb.AppendLine($"File 1 hash: {BlakeString1}");
+                        sb.AppendLine("");
+                        sb.AppendLine($"File 2 hash: {BlakeString2}");
+                        sb.AppendLine("");
+                        sb.AppendLine("Integrity check complete: Failure!");
+                        MessageBox.Show(sb.ToString(), "Integrity Checker");
+                    }
+                    sb.Clear();
                 }
-                sb.Clear();
+
+
+
+                
             }
             catch
             {
@@ -107,6 +152,11 @@ namespace EncryptieTool.Views
                 Console.WriteLine("No file selected.");
                 return null;
             }
+        }
+
+        private void CBHashingAG_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            HashingAlgor = ((ComboBoxItem)CBHashingAG.SelectedItem).Content.ToString();
         }
     }
 }
--- KeysLibrary/Hashing.cs
+++ KeysLibrary/Hashing.cs
@@ -5,15 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 using System.IO;
+using Blake3_Arctium;
+using Arctium.Cryptography.HashFunctions.Hashes;
+using Arctium.Shared.Helpers.Buffers;
 
 namespace KeysLibrary
 {
     public class Hashing
     {
 
-        public byte[] FileToHash(string filePath)
+        public byte[] FileToHashSHA(string filePath)
         {
-            if(File.Exists(filePath)) 
+            if (File.Exists(filePath))
             {
                 using (SHA256 mySHA256 = SHA256.Create())
                 {
@@ -34,7 +37,30 @@ namespace KeysLibrary
                     }
                 }
             }
-            return null;            
+            return null;
+        }
+        //documentation -> https://github.com/NeuroXiq/Arctium
+        public byte[] FileToHashBLAKE(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                try
+                {
+                    using (var stream = File.OpenRead(filePath))
+                    {
+                        BLAKE3 blake3 = new BLAKE3();
+                        byte[] byteArray = Encoding.UTF8.GetBytes(filePath);
+                        blake3.HashBytes(byteArray);
+                        byte[] computedHash = blake3.HashFinal();
+                        return computedHash;
+                    }
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            return null;
         }
     }
 }
